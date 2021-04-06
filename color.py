d85919f929f0c02bb756bc85740bfb7c493ca27f
alejandrasanchezv <73018886+alejandrasanchezv@users.noreply.github.com>
2021-04-06 18:02:43 +0200

    Organized the code, made it more readable

--- color.py
+++ color.py
@@ -5,14 +5,16 @@ import time
 
 pycom.heartbeat(False)
 
-#pycom.rgbled(0x5cf144) #Green
-#time.sleep(5)
-#pycom.rgbled(0xff0000) #Red
-#time.sleep(5)
-#pycom.rgbled(0x0000ff) #Blue
-#time.sleep(5)
-pycom.rgbled(0xf1447c) #pink
+pycom.rgbled(0x00ff00) #Green
 time.sleep(5) # 5 second wait
-pycom.rgbled(0xf77207) #orange
+pycom.rgbled(0xff0000) #Red
+time.sleep(5)
+pycom.rgbled(0x0000ff) #Blue
+time.sleep(5)
+
+#TESTING DIFFERENT COLORS
+#pycom.rgbled(0xf1447c) #pink
+#time.sleep(5)
+#pycom.rgbled(0xf77207) #orange
 
 pycom.heartbeat(True) #turn led into its initial state
