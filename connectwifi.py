9ce9c9f763f43e4b2bb761cfb12e3eeec3d62b6e
alejandrasanchezv <73018886+alejandrasanchezv@users.noreply.github.com>
2021-04-13 13:51:08 +0200

    new release

--- connectwifi.py
+++ connectwifi.py
@@ -4,19 +4,17 @@
 #Doubt: upload the firmware via Wi-Fi
 
 from network import WLAN
-import machine
 import pycom
 
 pycom.heartbeat(False) #activate LED
 
 wlan = WLAN()
-wlan.init(ssid="Home&Life SuperWiFi-D7A1", auth=(WLAN.WPA2, "PLD74M8HH8AGGYFB"))
-#connect to wifi
 
 while not wlan.isconnected():
     print("Unable to connect")
     pycom.rgbled(0xff0000) #Red
-    #machine.idle() #rest
+    wlan.init(ssid="Home&Life SuperWiFi-D7A1", auth=(WLAN.WPA2, "PLD74M8HH8AGGYFB"))
 
 pycom.rgbled(0x00ff00) #Green
 pybytes.send_signal(1,"Connected")
+print("Connected")
--- flash/main.py
+++ flash/main.py
@@ -1,3 +1,8 @@
+#Exercise 2
+#The Fipy connects to the wifi network
+#LED: red if it was unable to connect, green connected to wifi
+#Doubt: upload the firmware via Wi-Fi
+
 from network import WLAN
 import pycom
 
@@ -6,9 +11,10 @@ pycom.heartbeat(False) #activate LED
 wlan = WLAN()
 
 while not wlan.isconnected():
+    print("Unable to connect")
     pycom.rgbled(0xff0000) #Red
     wlan.init(ssid="Home&Life SuperWiFi-D7A1", auth=(WLAN.WPA2, "PLD74M8HH8AGGYFB"))
 
-
 pycom.rgbled(0x00ff00) #Green
 pybytes.send_signal(1,"Connected")
+print("Connected")
