0b323bc01dcd91a8e5ea08c0687d650a1966616d
alejandrasanchezv <73018886+alejandrasanchezv@users.noreply.github.com>
2021-04-13 12:33:33 +0200

    senf signal to pybytes

--- connectwifi.py
+++ connectwifi.py
@@ -16,7 +16,8 @@ wlan.init(ssid="Home&Life SuperWiFi-D7A1", auth=(WLAN.WPA2, "PLD74M8HH8AGGYFB"))
 while not wlan.isconnected():
     print("Unable to connect")
     pycom.rgbled(0xff0000) #Red
-    machine.idle() #rest
+    #machine.idle() #rest
 
 pycom.rgbled(0x00ff00) #Green
+pybytes.send_signal(1,"Connected")
 #print(wlan.ifconfig())
