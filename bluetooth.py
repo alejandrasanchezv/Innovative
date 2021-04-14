b517bf6768ca92e7f19ce8022945a3e89a2d6ed3
alejandrasanchezv <73018886+alejandrasanchezv@users.noreply.github.com>
2021-04-14 21:17:03 +0200

    It works

--- bluetooth.py
+++ bluetooth.py
@@ -1,28 +1,30 @@
 from network import Bluetooth
-import time
-bt = Bluetooth()
-bt.start_scan(-1)
+import ubinascii
 
+bluetooth = Bluetooth()
+bluetooth.set_advertisement(name='FiPy', service_uuid=b'1234567890123456')
+
+def conn_cb (bt_o):
+    events = bt_o.events()   # this method returns the flags and clears the internal registry
+    if events & Bluetooth.CLIENT_CONNECTED:
+        print("Client connected")
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        print("Client disconnected")
+
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+
+bluetooth.advertise(True)
+
+bluetooth.start_scan(-1)
+adv = None
 while True:
-  adv = bt.get_adv()
-  if adv and bt.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL) == 'Heart Rate':
-      try:
-          conn = bt.connect(adv.mac)
-          services = conn.services()
-          for service in services:
-              time.sleep(0.050)
-              if type(service.uuid()) == bytes:
-                  print('Reading chars from service = {}'.format(service.uuid()))
-              else:
-                  print('Reading chars from service = %x' % service.uuid())
-              chars = service.characteristics()
-              for char in chars:
-                  if (char.properties() & Bluetooth.PROP_READ):
-                      print('char {} value = {}'.format(char.uuid(), char.read()))
-          conn.disconnect()
-          break
-      except:
-          print("Error while connecting or reading from the BLE device")
-          break
-  else:
-      time.sleep(0.050)
+    adv = bluetooth.get_adv()
+    if adv:
+        try:
+            bluetooth.connect(adv.mac)
+        except:
+            # start scanning again
+            bluetooth.start_scan(-1)
+            continue
+        break
+#print("Connected to device with addr = {}".format(ubinascii.hexlify(adv.mac)))
